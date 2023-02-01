38131ef902053a2208a54f1572c3b85540231d85
Jose <jose.osorio.herrera@gmail.com>
2023-01-31 22:04:54 -0300

    avances en colores themas

--- src/components/atoms/ButtonCalc.jsx
+++ src/components/atoms/ButtonCalc.jsx
@@ -2,8 +2,11 @@ const ButtonCalc = ({objBtn, clickAction}) => {
     const handleClickAction = (data) => {
         clickAction(data)
     }
+    const delClass = objBtn.text === 'RESET' || objBtn.text === 'DEL' ? "delete" : null;
+    const eqClass = objBtn.text === '=' ? "equal" : null;
+    
     return (
-        <button className="button__calc" onClick={() => handleClickAction(objBtn)}>
+        <button className={['button__calc',delClass, eqClass].join(' ')} onClick={() => handleClickAction(objBtn)}>
             {objBtn.text}
         </button>
     )
